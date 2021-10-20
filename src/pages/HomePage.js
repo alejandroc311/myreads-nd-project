b0fb1a4c667101105c2d92ffb07f5427efa49022
Alejandro Crespo Mejías <alejandroc311@gmail.com>
2021-10-20 14:55:48 -0400

    test: Test deconstruction of props object

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,22 @@
 import { update } from "../BooksAPI";
 function HomePage({bookList}){
-    console.log("Home Page Test:", bookList);
+    const books = bookList.map(({id, shelf})=>{
+        return(
+            <h2>
+                Book Id: {id} and Book Status: {shelf}
+            </h2>
+        );
+    });
     return(
-        <h2>Home Page</h2>
+        <div className="container-fluid">
+            <div className="row">
+                <div className="col">
+                    <div className="card">
+                        {books}
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 
