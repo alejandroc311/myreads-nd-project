f55caa382ab6dec5a925cdcdaa7810b3afe2485b
Alejandro Crespo Mejías <alejandroc311@gmail.com>
2021-10-19 22:18:00 -0400

    feat: Add Router, Switch, and Route in App component
    Each Route will render the two main components: A "homePage" component and a "searchPage" component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { useState, useEffect } from "react";
+import HomePage from "./pages/HomePage";
+import SearchPage from "./pages/SearchPage";
 import './App.css';
 
 function App() {
+  const [bookLists, setBookLists] = useState([]);
+
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route  exact path="/" render={<HomePage bookLists={}/>}/>
+        <Route path="/search" render={<SearchPage bookLists={}/>}/>
+      </Switch>
+    </Router>
   );
 }
 
